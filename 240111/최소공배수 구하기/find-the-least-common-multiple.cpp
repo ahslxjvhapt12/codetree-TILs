a02d9518b853aa03577550ef6143f9b029694a4a
ahslxjvhapt12 <102218989+ahslxjvhapt12@users.noreply.github.com>
2024-01-11 08:31:58 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+	int n;
+
+	while (b != 0) {
+		n = a % b;
+		a = b;
+		b = n;
+	}
+
+	return a;
+}
+
+int lcm(int a, int b) {
+	return a * b / gcd(a, b);
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << lcm(a, b);
+}
