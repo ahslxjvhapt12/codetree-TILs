9c09b19bea8c3d8535ace24796d62301568e4252
ahslxjvhapt12 <102218989+ahslxjvhapt12@users.noreply.github.com>
2024-01-11 11:17:10 +0900

    Title: 007, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/007/007.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str1,str2,str3;
+
+    cin >> str1 >> str2>>str3;
+    cout << "secret code : " << str1 << endl;
+    cout << "meeting point : " << str2 << endl;
+    cout << "time : " << str3 << endl;
+    
+}
