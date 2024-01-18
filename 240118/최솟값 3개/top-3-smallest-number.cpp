a79ac3fb6cf03bccc97660e1fa00cc4e919dca32
ahslxjvhapt12 <102218989+ahslxjvhapt12@users.noreply.github.com>
2024-01-18 11:16:43 +0900

    Title: 최솟값 3개, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240118/최솟값 3개/top-3-smallest-number.cpp
+++ 240118/최솟값 3개/top-3-smallest-number.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int main() {
-	vector<long long> v;
-	long long n, input;
+
+	priority_queue<int, vector<int>, greater<int>> q;
+
+	int n, input;
+
 	cin >> n;
 
-	for (long long i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cin >> input;
-		v.push_back(input);
-		if (v.size() < 3) {
-			cout << -1 << '\n';
+		q.push(input);
+
+		if (q.size() < 3) {
+			cout << -1;
 		}
 		else {
-			sort(v.begin(), v.end());
-			cout << v[0] * v[1] * v[2] << '\n';
+			int n1, n2, n3;
+			n1 = q.top();
+			q.pop();
+			n2 = q.top();
+			q.pop();
+			n3 = q.top();
+			q.pop();
+
+			cout << n1 * n2 * n3;
+			q.push(n1);
+			q.push(n2);
+			q.push(n3);
 		}
+
+		cout << '\n';
 	}
 }
